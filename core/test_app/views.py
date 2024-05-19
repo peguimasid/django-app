8c07d812804b6304062b1350fc78dd2575d57139
Guilhermo Masid <guilhermomasid@gmail.com>
2024-05-18 22:00:21 -0300

    just basic

--- core/test_app/views.py
+++ core/test_app/views.py
@@ -1,24 +1,7 @@
-from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
-
-from core.test_app.serializers import GroupSerializer, UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
-    queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
-    queryset = Group.objects.all().order_by("name")
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(["GET"])
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
--- core/urls.py
+++ core/urls.py
@@ -17,16 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
-from rest_framework import routers
-
 from core.test_app import views
 
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
-
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", views.hello_world),
     path("admin/", admin.site.urls),
 ]
